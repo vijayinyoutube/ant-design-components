36c3031a610de45f0a0446c59f05f5799547bfdd
vijayinyoutube <vijaythevirtue@gmail.com>
2024-01-30 06:58:27 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Layout, Menu, theme } from "antd";
+import { Header, Content, Footer } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import React from "react";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { Header, Content, Footer, Sider } = Layout;
+
+  const items = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    UserOutlined,
+  ].map((icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+  }));
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Layout className="h-screen">
+        <Sider
+        trigger={<></>}
+          reverseArrow={true}
+          width={250}
+          breakpoint="md"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["4"]}
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              className="h-full"
+              style={{
+                padding: 24,
+                minHeight: 360,
 
-export default App
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }} className="bottom-0">
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
+
+export default App;
